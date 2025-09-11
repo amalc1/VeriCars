dc50ecc206e5ac1609cd02224af771758d366b3e
amalc <amalc.plr@gmail.com>
2025-09-12 00:57:23 +0530

    feat: TestDriveForm and

---
+++ app/(main)/reservations/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "My Reservations | Vericars",
+  description: "Manage your test drive reservations",
+};
+
+const ReservationsPage = async () => {
+  return <div>ReservationsPage</div>;
+};
+
+export default ReservationsPage;
---
+++ app/(main)/test-drive/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { getCarById } from "@/actions/car-listing";
+import { notFound } from "next/navigation";
+import TestDriveForm from "./_components/TestDriveForm";
+
+export async function generateMetadata() {
+  return {
+    title: `Book Test Drive | Vericars`,
+    description: `Schedule a test drive in few seconds`,
+  };
+}
+
+const TestDrivePage = async ({ params }: TestDrivePageProps) => {
+  const { id } = params;
+  const result = await getCarById(id);
+
+  // If car not found, 404
+  if (!result.success) {
+    notFound();
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-6 gradient-title">Book a Test Drive</h1>
+      <TestDriveForm
+        car={result.data}
+        testDriveInfo={result.data.testDriveInfo}
+      />
+    </div>
+  );
+};
+
+export default TestDrivePage;
+
+type TestDrivePageProps = {
+  params: {
+    id: string;
+  };
+};
